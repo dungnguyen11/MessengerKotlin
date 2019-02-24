e585390ff5898aa61b1e8536c57b337c9adf4bb7
Dung Nguyen <nguyentridung165@gmail.com>
2019-02-25 03:26:27 +0700

    Done Part 1

---
+++ app/src/main/java/com/dungnguyen/messengerkotlin/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.dungnguyen.messengerkotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        backtologin_textview_login.setOnClickListener {
+            finish()
+        }
+    }
+}
