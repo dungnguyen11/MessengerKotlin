f649dcc2117f67c3f1c69f82e318a3cde45250a3
Dung Nguyen <nguyentridung165@gmail.com>
2019-02-25 04:30:37 +0700

    Done Part 2

--- app/src/main/java/com/dungnguyen/messengerkotlin/LoginActivity.kt
+++ app/src/main/java/com/dungnguyen/messengerkotlin/LoginActivity.kt
@@ -2,12 +2,31 @@ package com.dungnguyen.messengerkotlin
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val TAG = "LoginActivity"
+
+        login_button_login.setOnClickListener {
+            val email = email_edittext_login.text.toString()
+            val password = password_edittext_login.text.toString()
+
+            Log.d(TAG, "Email: $email, Password: $password")
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+
+                }
+                .addOnFailureListener {
+
+                }
+        }
 
         backtologin_textview_login.setOnClickListener {
             finish()
--- app/src/main/java/com/dungnguyen/messengerkotlin/MainActivity.kt
+++ app/src/main/java/com/dungnguyen/messengerkotlin/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,11 +15,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         register_button_register.setOnClickListener {
-            val email = email_edittext_register.text.toString()
-            val password = password_edittext_register.text.toString()
-
-            Log.d("MainActivity", "Email is: " + email)
-            Log.d("MainActivity", "Password is: $password")
+            performRegister()
         }
 
         already_have_account_textview.setOnClickListener {
@@ -27,5 +25,32 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun performRegister() {
+        val TAG = "MainActivity"
+        val email = email_edittext_register.text.toString()
+        val password = password_edittext_register.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter text in email, and password", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        Log.d(TAG, "Email is: " + email)
+        Log.d(TAG, "Password is: $password")
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+
+                // else if successful
+                Log.d("MainActivity",
+                    "Successfully created user with uid: ${it.result?.user?.uid}")
+            }
+            .addOnFailureListener {
+                Log.d(TAG, "Fail to create user: ${it.message}")
+                Toast.makeText(this, it.message.toString(), Toast.LENGTH_SHORT).show()
+            }
     }
 }
